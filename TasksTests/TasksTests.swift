04c92dda1e3e04311dfef59c2b172d9b87f473f2
sudolibre <optout.breaker@gmail.com>
2017-01-11 10:01:59 -0500

    adding some tests

--- TasksTests/TasksTests.swift
+++ TasksTests/TasksTests.swift
@@ -11,26 +11,44 @@ import XCTest
 
 class TasksTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testAddTask() {
+        let taskStore = TaskStore()
+        taskStore.addTask(Task(title: "firstTask"))
+        taskStore.addTask(Task(title: "secondTask"))
+        taskStore.addTask(Task(title: "thirdTask"))
+        XCTAssertTrue(taskStore.tasks.first!.title == "firstTask")
+        XCTAssertTrue(taskStore.tasks[1].title == "secondTask")
+        XCTAssertTrue(taskStore.tasks.last!.title == "thirdTask")
+        
+        
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testUpdateTask() {
+        let taskStore = TaskStore()
+        taskStore.addTask(Task(title: "firstTask"))
+        taskStore.addTask(Task(title: "secondTask"))
+        taskStore.addTask(Task(title: "thirdTask"))
+        taskStore.updateTask(taskStore.tasks.first!, withDescription: "updatedFirst")
+        XCTAssertTrue(taskStore.tasks.first!.title == "updatedFirst")
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testDeleteTask() {
+        let taskStore = TaskStore()
+        taskStore.addTask(Task(title: "firstTask"))
+        taskStore.addTask(Task(title: "secondTask"))
+        taskStore.addTask(Task(title: "thirdTask"))
+        taskStore.deleteTask(taskStore.tasks[1])
+        XCTAssertTrue(taskStore.tasks[1].title == "thirdTask")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testMoveTask() {
+        let taskStore = TaskStore()
+        taskStore.addTask(Task(title: "firstTask"))
+        taskStore.addTask(Task(title: "secondTask"))
+        taskStore.addTask(Task(title: "thirdTask"))
+        taskStore.moveItem(from: 2, to: 1)
+        taskStore.moveItem(from: 0, to: 2)
+        XCTAssertTrue(taskStore.tasks.map({$0.title}) == ["thirdTask", "secondTask", "firstTask"])
     }
     
 }
